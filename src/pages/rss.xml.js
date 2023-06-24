98d656b780fae47f50087cd4a15f0c32094e3ddc
hadesxiong <fengshixiong890614@gmail.com>
2023-06-24 11:22:09 +0800

    mark rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,30 +1,30 @@
-import rss from '@astrojs/rss';
+// import rss from '@astrojs/rss';
 
-let allPosts = import.meta.glob('./posts/*.md', { eager: true });
-let posts = Object.values(allPosts);
-posts = posts.sort((a, b) => {
-  return (
-    parseInt(b.url.split('/posts/')[1].split('-')[0]) -
-    parseInt(a.url.split('/posts/')[1].split('-')[0])
-  );
-});
+// let allPosts = import.meta.glob('./posts/*.md', { eager: true });
+// let posts = Object.values(allPosts);
+// posts = posts.sort((a, b) => {
+//   return (
+//     parseInt(b.url.split('/posts/')[1].split('-')[0]) -
+//     parseInt(a.url.split('/posts/')[1].split('-')[0])
+//   );
+// });
 
-export const get = () =>
-  rss({
-    title: '--',
-    description: '--',
-    site: '',
-    customData: `<image><url>https://pmfeeds.bearman.xyz/weekly/noodles.png</url></image>`,
-    items: posts.map((item) => {
-      const url = item.url;
-      const oldTitle = url.split('/posts/')[1];
-      const title =
-        '第' + oldTitle.split('-')[0] + '期 - ' + oldTitle.split('-')[1];
-      return {
-        link: url,
-        title,
-        description: item.frontmatter.content,
-        pubDate: item.frontmatter.date,
-      };
-    }),
-  });
+// export const get = () =>
+//   rss({
+//     title: '--',
+//     description: '--',
+//     site: '',
+//     customData: `<image><url>https://pmfeeds.bearman.xyz/weekly/noodles.png</url></image>`,
+//     items: posts.map((item) => {
+//       const url = item.url;
+//       const oldTitle = url.split('/posts/')[1];
+//       const title =
+//         '第' + oldTitle.split('-')[0] + '期 - ' + oldTitle.split('-')[1];
+//       return {
+//         link: url,
+//         title,
+//         description: item.frontmatter.content,
+//         pubDate: item.frontmatter.date,
+//       };
+//     }),
+//   });
