6b101010a8061c16d069fed4825671dd124dfb21
hadesxiong <fengshixiong890614@gmail.com>
2023-06-24 13:58:01 +0800

    update files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site:"https://hadesxiong.github.io",
-    base:"/pm-weekly"
+    // site:"https://hadesxiong.github.io",
+    // base:"/pm-weekly"
 });
